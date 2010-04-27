dcfaa4bc29e6b53452424b6a28159e8f634aafbe
Dmitry Ratnikov <ratnikov@gmail.com>
2010-04-28 06:16:15 +0800

    Seems like active_record/core_ext need to be manually required to allow I18n.with_options

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'test/unit'
 require 'active_support'
+require 'active_support/core_ext'
 require 'action_view'
 require 'action_controller'
 require 'action_controller/test_case'
